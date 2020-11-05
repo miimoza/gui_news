861c387d47b9592c189263494e8e989dbd8cd591
Antoine Coulon <antoine.coulon@epita.fr>
2020-11-05 08:38:49 +0100

    asd

---
+++ main.py
@@ -0,0 +1,20 @@
+import subprocess
+import importlib
+import gui_news
+import display
+import time
+
+while True:
+	#subprocess.check_call(["git","pull"])
+	importlib.reload(gui_news)
+	importlib.reload(display)
+
+
+	gui_news.main()
+
+
+	display.move_cursor(28, 0)
+	print("="*80)
+	for i in range(0, 80):
+	    print('.',end='', flush=True)
+	    time.sleep(0.2)
