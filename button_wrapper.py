20474cbea03c0a3be0ddc9dfe92c02a7e8ceab5f
Antoine Coulon <antoine.coulon@epita.fr>
2020-11-05 08:28:17 +0100

    tas

---
+++ button_wrapper.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+import subprocess
+from threading import Thread
+from datetime import datetime
+
+def main():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    thread_button_1 = Thread(target = wrapper, args = (18,action_motor))
+
+    thread_button_1.start()
+    print("[button_wrapper] Launch Thread function")
+
+def wrapper(gpio_number, function):
+    GPIO.setup(gpio_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    while True:
+        r = GPIO.input(gpio_number)
+        if r == False:
+            function()
+
+def action_motor():
+    print("ca marche bien")
