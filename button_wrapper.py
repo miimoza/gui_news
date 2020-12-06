ae742bdd89588a44d42a9084965523031f854bc9
Antoine Coulon <antoine.coulon@epita.fr>
2020-12-06 21:02:21 +0100

    asD

--- button_wrapper.py
+++ button_wrapper.py
@@ -4,12 +4,14 @@ import subprocess
 from threading import Thread
 from datetime import datetime
 import motor
+import os
 
 def main():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
     thread_button_1 = Thread(target = wrapper, args = (5,action_motor))
+    thread_button_2 = Thread(target = wrapper, args = (2,apply_reboot))
 
     thread_button_1.start()
     print("[button_wrapper] Launch Thread function")
@@ -24,3 +26,6 @@ def wrapper(gpio_number, function):
 def action_motor():
     motor.steps(-300)
     motor.steps(300)
+
+def apply_reboot():
+    os.system("sudo reboot")
