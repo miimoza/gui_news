1342dda2df1b32f66f76b1bd4b23624e3f133f9c
Antoine Coulon <antoine.coulon@epita.fr>
2021-11-08 01:06:42 +0100

    ok

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
 
 
 	display.move_cursor(62, 0)
-	print("="*125)
-	for i in range(0, 125):
+	print("="*127)
+	for i in range(0, 127):
 	    print('.',end='', flush=True)
 	    time.sleep(1)
